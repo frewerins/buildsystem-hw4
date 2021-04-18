6a46428e26c2be62a45c4d3831ac528aead33678
Knyazeva Anastasia <kniazeva.ad@phystech.edu>
2021-04-18 20:53:59 +0300

    task

---
+++ way1/main.cpp
@@ -0,0 +1,19 @@
+#include <opencv2/core.hpp>
+#include <string>
+#include <opencv2/imgcodecs.hpp>
+
+int main() {
+    auto map = cv::imread("../../data/map.png");
+    auto file = cv::imread("../../data/file.png");
+    for (size_t i = 0; i < file.rows; ++i) {
+        for (size_t j = 0; j < file.cols; ++j) {
+            auto& item = file.at<cv::Vec3b>(i, j);
+            for (int k = 0; ch < file.channels(); ++k) {
+                auto result = map.at<cv::Vec3b>(abs(file.channels() - (k + 1)), item[k]);
+                item[k] = result[k];
+            }
+        }
+    }
+    cv::imwrite("../../data/result1.png", file);
+    return 0;
+}
