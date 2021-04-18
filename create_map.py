982fe9a5c0b4475fd8bc254803ee831f5fef1405
Knyazeva Anastasia <kniazeva.ad@phystech.edu>
2021-04-18 21:16:19 +0300

    task

---
+++ create_map.py
@@ -0,0 +1,31 @@
+import cv2
+
+if __name__ == "__main__":
+    print("122")
+    with open("map.h", "w+") as f:
+        f.write(
+            "#pragma once\n"
+            "#include <string>\n"
+            "#include <vector>\n"
+            "std::vector<std::vector<std::vector<u_char>>> map = {\n"
+        )
+        map = cv2.imread("../data/map.png")
+        for i in range(map.shape[0]):
+            f.write("\t{\n")
+            for j in range(map.shape[1]):
+                f.write("\t\t{ ")
+                for k in range(map.shape[2]):
+                    if k == len(map.shape[2]) - 1:
+                        f.write(str(map[i, j, k]) + " ")
+                    else:
+                        f.write(str(map[i, j, k]) + ", ")
+                if j == len(map.shape[1]) - 1:
+                    f.write("}\n")
+                else:
+                    f.write("},\n")
+            if i == len(map.shape[0]):
+                f.write("\t}\n")
+            else:
+                f.write("\t},\n")
+        f.write("};\n")
+        f.close()
